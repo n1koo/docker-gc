067f10dca9e899cd0261d218dc0143eba878f312
n1koo <niko.kurtti@shopify.com>
2016-02-25 17:10:58 +0200

    first commit

---
+++ gc.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+  log "github.com/Sirupsen/logrus"
+  "github.com/fsouza/go-dockerclient"
+  "os"
+  "time"
+  "sort"
+  "github.com/n1koo/gocron"
+)
+
+const dockerEndpoint = "unix:///var/run/docker.sock"
+
+func startDockerClient(endpoint ...string) *docker.Client {
+  var dEndpoint string
+  if len(endpoint) == 0 {
+    dEndpoint = dockerEndpoint
+  } else {
+    dEndpoint = endpoint[0]
+  }
+
+  client, err := docker.NewClient(dEndpoint)
+  if err != nil {
+    log.Fatal(err)
+    os.Exit(1)
+  }
+  return client
+}
+
+func continuousGC(intervalInSeconds uint64, keepLastContainers time.Duration, keepLastImages time.Duration, client *docker.Client) {
+  gocron.Every(intervalInSeconds).Seconds().Do(cleanAll, keepLastContainers, keepLastImages, client)
+  log.Info("Continous run started with interval (in seconds): ", intervalInSeconds)
+  gocron.Start()
+}
+
+func stopGC() {
+  gocron.Clear()
+}
+
+func cleanAll(keepLastContainers time.Duration, keepLastImages time.Duration, client *docker.Client) {
+  log.Info("Cleaning all images/containers")
+  cleanContainers(keepLastContainers, client)
+  cleanImages(keepLastImages, client)
+}
+
+func cleanContainers(keepLast time.Duration, client *docker.Client) {
+  conts, err := client.ListContainers(docker.ListContainersOptions{All: true})
+  if err != nil {
+    log.Error("Listing containers error: ", err)
+  }
+
+  containerMap := map[string]int64{}
+  for _, cont := range conts {
+    containerMap[cont.ID] = cont.Created
+  }
+
+  removeData(containerMap, "container", keepLast, client)
+}
+
+func cleanImages(keepLast time.Duration, client *docker.Client) {
+  imgs, err := client.ListImages(docker.ListImagesOptions{All: true})
+  if err != nil {
+    log.Error("Listing images error: ", err)
+  }
+
+  imageMap := map[string]int64{}
+  for _, img := range imgs {
+    imageMap[img.ID] = img.Created
+  }
+
+  removeData(imageMap, "image", keepLast, client)
+}
+
+func removeData(dataMap map[string]int64, dataType string, keepLast time.Duration, client *docker.Client) {
+  //Sort map keys to make deletion order predictable
+  var ids []string
+  for k := range dataMap {
+    ids = append(ids, k)
+  }
+  sort.Strings(ids)
+
+  for _, id := range ids {
+    ageOfData := time.Since(time.Unix(dataMap[id], 0))
+
+    // If container/image is older than our threshold, delete it
+    if ageOfData > keepLast {
+      log.WithFields(log.Fields{
+        dataType + " too old by": ageOfData - keepLast,
+        dataType + " age":        ageOfData,
+        "we keep last":           keepLast,
+      }).Info("Trying to delete "+dataType+": ", id)
+
+      if dataType == "image" {
+        err := client.RemoveImage(id)
+        if err != nil {
+          log.WithField("error", err).Error("Image deletion error for: ", id)
+        }
+      } else if dataType == "container" {
+        opts := docker.RemoveContainerOptions{ID: id}
+        err := client.RemoveContainer(opts)
+        if err != nil {
+          log.WithField("error", err).Error("Container deletion error for: ", id)
+        }
+      } else {
+        log.Error("removeData called with unvalid Datatype: " + dataType)
+      }
+    }
+  }
+}
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  log "github.com/Sirupsen/logrus"
+  "os"
+  "time"
+)
+
+var (
+  KeepLastImages     time.Duration
+  KeepLastContainers time.Duration
+  Command            string
+  IntervalForContinuousMode time.Duration
+)
+
+var (
+  commandFlag            = flag.String("command", "continuous", "What to clean (images|containers|all|emergency|continous")
+  keepLastImagesFlag     = flag.Duration("keep_last_images", 10*time.Hour, "How old images are kept")
+  keepLastContainersFlag = flag.Duration("keep_last_containers", 1*time.Minute, "How old containers are kept")
+  intervalForContinuousMode = flag.Duration("interval", 60*time.Second, "How old containers are kept")
+)
+
+const usageMessage = `Usage of 'docker-gc':
+  docker-gc (-command=containers|images|all|emergency) (-keep_last_images=DURATION) (-keep_last_containers=DURATION)
+  -command=all cleans all images and containes respecting keep_last values
+  -command=emergency same as all, but with 0second keep_last values
+  OR
+  docker-gc (-command=continuous) (-interval=INTERVAL_IN_SECONDS) (-keep_last_images=DURATION) (-keep_last_containers=DURATION) for continuous cleanup
+`
+
+func main() {
+  parseFlags()
+  client := startDockerClient()
+
+  switch Command {
+  case "images":
+    cleanImages(KeepLastImages, client)
+  case "containers":
+    cleanContainers(KeepLastContainers, client)
+  case "all":
+    cleanContainers(KeepLastContainers, client)
+    cleanImages(KeepLastImages, client)
+  case "emergency":
+    cleanContainers(0*time.Second, client)
+    cleanImages(0*time.Second, client)
+  case "continuous":
+    interval := uint64(IntervalForContinuousMode.Seconds())
+    continuousGC(interval, KeepLastContainers, KeepLastImages, client)
+    select{}
+  default:
+    log.Error("%q is not valid command.\n", Command)
+    os.Exit(2)
+  }
+}
+
+// Usage is a replacement usage function for the flags package.
+func Usage() {
+  fmt.Fprintln(os.Stderr, usageMessage)
+  fmt.Fprintln(os.Stderr, "Flags:")
+  flag.PrintDefaults()
+  os.Exit(2)
+}
+
+func parseFlags() {
+  flag.Usage = Usage
+  flag.Parse()
+
+  Command = *commandFlag
+  KeepLastImages = *keepLastImagesFlag
+  KeepLastContainers = *keepLastContainersFlag
+  IntervalForContinuousMode = *intervalForContinuousMode
+
+  if Command != "all" && Command != "images" && Command != "containers" && Command != "emergency" && Command != "continuous" {
+    flag.Usage()
+    os.Exit(2)
+  }
+}
